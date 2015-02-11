b9c61cefd02251b06f6ba1a0cbef55e8b756407b
waylau <waylau521@gmail.com>
2015-02-11 18:07:17 +0800

    addd

---
+++ netty4-demos/src/main/java/com/waylau/netty/echo/EchoClientHandler.java
@@ -0,0 +1,44 @@
+package com.waylau.netty.echo;
+
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+ private final ByteBuf firstMessage;
+ 
+    /**
+      * Creates a client-side handler.
+       */
+      public EchoClientHandler() {
+         firstMessage = Unpooled.buffer(EchoClient.SIZE);
+         for (int i = 0; i < firstMessage.capacity(); i ++) {
+              firstMessage.writeByte((byte) i);
+         }
+      }
+  
+      @Override
+      public void channelActive(ChannelHandlerContext ctx) {
+          ctx.writeAndFlush(firstMessage);
+      }
+  
+      @Override
+      public void channelRead(ChannelHandlerContext ctx, Object msg) {
+         ctx.write(msg);
+      }
+  
+     @Override
+      public void channelReadComplete(ChannelHandlerContext ctx) {
+         ctx.flush();
+      }
+  
+     @Override
+     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {         // Close the connection when an exception is raised.
+          cause.printStackTrace();
+         ctx.close();
+      }
+  }
---
+++ netty4-demos/src/main/java/com/waylau/netty/echo/EchoServerHandler.java
@@ -0,0 +1,23 @@
+package com.waylau.netty.echo;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 处理服务端 channel.
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { 
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { 
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
