2d63097aa7ac1b0599788030594c972536dcb3e3
waylau <waylau521@gmail.com>
2015-03-12 17:36:20 +0800

    update

--- netty4-demos/src/main/java/com/waylau/netty/demo/websocketchat/WebsocketChatServerInitializer.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/websocketchat/WebsocketChatServerInitializer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -22,9 +24,8 @@ public class WebsocketChatServerInitializer extends
     public void initChannel(SocketChannel ch) throws Exception {
 		 ChannelPipeline pipeline = ch.pipeline();
 
-        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
-        pipeline.addLast("decoder", new StringDecoder());
-        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast("handler", new WebsocketChatServerHandler());
  
 		System.out.println("SimpleChatClient:"+ch.remoteAddress() +"连接上");
