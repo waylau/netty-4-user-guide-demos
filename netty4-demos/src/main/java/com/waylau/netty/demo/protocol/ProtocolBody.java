4c4577628734ca57b87902c84e10a85bc168233b
waylau <waylau521@gmail.com>
2015-11-04 19:57:19 +0800

    add  协议

---
+++ netty4-demos/src/main/java/com/waylau/netty/demo/protocol/ProtocolBody.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.waylau.netty.demo.protocol;
+
+/**
+ * 说明：协议消息体
+ *
+ * @author <a href="http://www.waylau.com">waylau.com</a> 2015年11月4日 
+ */
+public class ProtocolBody {
+
+	/**
+	 * 
+	 */
+	public ProtocolBody() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ netty4-demos/src/main/java/com/waylau/netty/demo/protocol/ProtocolHeader.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.waylau.netty.demo.protocol;
+
+/**
+ * 说明：协议消息头
+ *
+ * @author <a href="http://www.waylau.com">waylau.com</a> 2015年11月4日 
+ */
+public class ProtocolHeader {
+
+	/**
+	 * 
+	 */
+	public ProtocolHeader() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
