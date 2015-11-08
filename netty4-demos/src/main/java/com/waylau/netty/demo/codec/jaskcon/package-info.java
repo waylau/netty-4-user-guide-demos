c3ec18e3f9b4854461ee73be03a80e5c13bfcdcf
waylau <waylau521@gmail.com>
2015-11-08 22:52:41 +0800

    add jackson demos

---
+++ netty4-demos/src/main/java/com/waylau/netty/demo/codec/jaskcon/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 说明：使用 jackson 来执行序列化
+ * 参考 	http://www.mkyong.com/java/jackson-2-convert-java-object-to-from-json/
+ * @author <a href="http://www.waylau.com">waylau.com</a>  2015年11月8日
+ */
+package com.waylau.netty.demo.codec.jaskcon;
