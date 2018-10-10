0de98fea931aaaf8e4f2ab6286a08f4f42e89d67
fan.shuai <fan.shuai@immomo.com>
2018-10-10 11:10:28 +0800

    update echo demo

--- netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoClientHandler.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoClientHandler.java
@@ -9,36 +9,42 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
- private final ByteBuf firstMessage;
+ private final String firstMessage;
  
     /**
       * Creates a client-side handler.
        */
       public EchoClientHandler() {
-         firstMessage = Unpooled.buffer(EchoClient.SIZE);
-         for (int i = 0; i < firstMessage.capacity(); i ++) {
-              firstMessage.writeByte((byte) i);
-         }
+//         firstMessage = Unpooled.buffer(EchoClient.SIZE);
+//         for (int i = 0; i < firstMessage.capacity(); i ++) {
+//              firstMessage.writeByte((byte) i);
+//         }
+////         firstMessage.writeByte('\n');
+          firstMessage = "hello\n";
       }
   
       @Override
       public void channelActive(ChannelHandlerContext ctx) {
           ctx.writeAndFlush(firstMessage);
+          System.out.println("channel active.");
       }
   
       @Override
       public void channelRead(ChannelHandlerContext ctx, Object msg) {
-         ctx.write(msg);
+          System.out.println("channel read from server: " + msg);
+         ctx.write(msg + "\n");
       }
   
      @Override
       public void channelReadComplete(ChannelHandlerContext ctx) {
          ctx.flush();
+         System.out.println("channel read complete");
       }
   
      @Override
-     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {         // Close the connection when an exception is raised.
-          cause.printStackTrace();
+     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+          // Close the connection when an exception is raised.
+         cause.printStackTrace();
          ctx.close();
       }
   }
--- netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoServerHandler.java
+++ netty4-demos/src/main/java/com/waylau/netty/demo/echo/EchoServerHandler.java
@@ -1,7 +1,11 @@
 package com.waylau.netty.demo.echo;
 
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 处理服务端 channel.
@@ -9,10 +13,27 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { 
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
     	System.out.println(ctx.channel().remoteAddress()+"->Server :"+ msg.toString());
-        ctx.write(msg); // (1)
-        ctx.flush(); // (2)
+//        ctx.write(msg); // (1)
+//        ctx.flush(); // (2)
+//        final ChannelFuture future = ctx.writeAndFlush(msg);
+        final ChannelFuture future = ctx.write(msg + "\n");
+//        future.addListener(ChannelFutureListener.CLOSE);
+//        future.addListener(new ChannelFutureListener() {
+//            @Override
+//            public void operationComplete(ChannelFuture future) throws Exception {
+//                System.out.println("close channel");
+//                future.channel().close();
+//            }
+//        });
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("server read complete");
+        ctx.flush();
+        TimeUnit.MILLISECONDS.sleep(200);
     }
 
     @Override
